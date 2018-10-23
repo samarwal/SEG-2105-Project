a6188c7a51a89423d8421d32d87e55b32ca23a14
Theo VdB <theo.a.vdb@gmail.com>
2018-10-23 18:39:23 -0400

    finished main_Activity with working transition buttons. Still need to add the logo

--- app/src/main/java/com/example/group/ondemandhomerepair/MainActivity.java
+++ app/src/main/java/com/example/group/ondemandhomerepair/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.group.ondemandhomerepair;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openLogin();
+            }
+        });
+        Button RegistrationButton = (Button) findViewById(R.id.registerButton);
+        RegistrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openRegistration();
+            }
+        });
+    }
+        public void openLogin(){
+            Intent intent = new Intent(this, LogIn.class);
+            startActivity(intent);
+        }
+        public void openRegistration(){
+        Intent intent = new Intent(this, Registration.class);
+        startActivity(intent);
     }
 }
+
