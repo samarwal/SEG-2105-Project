b7650dacf80893020345066734f7e0e9179faf9f
Logan Brown <lbrow077@uottawa.ca>
2018-10-23 22:45:58 -0400

    Updated layout of the Login activity. The Login button will now send the user to the welcome page when it is pressed.

--- app/src/main/java/com/example/group/ondemandhomerepair/LogIn.java
+++ app/src/main/java/com/example/group/ondemandhomerepair/LogIn.java
@@ -2,6 +2,9 @@ package com.example.group.ondemandhomerepair;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class LogIn extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        configureLogin();
+    }
+
+    private void condfigureLogin(){
+        Button loginButton = (Button) findViewById(R.id.btnLogIn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            public void OnClick(View view){
+                startActivity(new Intent(LogIn.this, WelcomePage.class));
+            }
+        });
     }
 }
