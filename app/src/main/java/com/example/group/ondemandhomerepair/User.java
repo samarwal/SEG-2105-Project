22cc736eaad6021e64ab6227d9d9436e1c47ca8c
Samarth Agarwal <32052305+samarwal@users.noreply.github.com>
2018-10-24 16:13:03 -0500

    Added User class

---
+++ app/src/main/java/com/example/group/ondemandhomerepair/User.java
@@ -0,0 +1,21 @@
+package com.example.group.ondemandhomerepair;
+
+public class User {
+
+    public String username, roleType;
+
+    public User(){
+    }
+
+    public User(String username, String roleType) {
+        this.username = username;
+        this.roleType = roleType;
+    }
+
+
+
+}
+
+
+
+
