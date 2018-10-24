d5184b430d22a9d32221e2353927cc9ec9dcdbcc
Samarth Agarwal <32052305+samarwal@users.noreply.github.com>
2018-10-24 14:55:08 -0500

    Added Firebase Auth to login

--- app/src/main/java/com/example/group/ondemandhomerepair/LogIn.java
+++ app/src/main/java/com/example/group/ondemandhomerepair/LogIn.java
@@ -1,5 +1,6 @@
 package com.example.group.ondemandhomerepair;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
@@ -9,12 +10,20 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogIn extends AppCompatActivity {
     private EditText username, password;
     private RadioButton radButton;
     private RadioGroup radGroup;
     private TextView errorMessage;
+    private FirebaseAuth firebaseAuth;
     public static final String EXTRA_TEXT1 = "com.example.group.ondemandhomerepair.EXTRA_TEXT1";
     public static final String EXTRA_TEXT2 = "com.example.group.ondemandhomerepair.EXTRA_TEXT2";
 
@@ -27,10 +36,19 @@ public class LogIn extends AppCompatActivity {
         radGroup = findViewById(R.id.radioGroupLogin);
         errorMessage = findViewById(R.id.errorMessageLogin);
         Button loginButton = (Button) findViewById(R.id.btnLogIn);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        /*if(user !=null){
+            finish();
+            startActivity (new Intent(LogIn.this, WelcomePage.class) );
+        }*/
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                configureLogin();
+                validate(username.getText().toString(), password.getText().toString());
             }
         });
     }
@@ -68,6 +86,22 @@ public class LogIn extends AppCompatActivity {
             radButton = findViewById(radGroup.getCheckedRadioButtonId());
         }
         return true;
+    }
+
+    public void validate(String userName, String userPassword){
+
+        firebaseAuth.createUserWithEmailAndPassword(userName,userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LogIn.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    startActivity (new Intent(LogIn.this, WelcomePage.class) );
+
+                }else{
+                    Toast.makeText(LogIn.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
 
