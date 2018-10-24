3bd112bd2e1e001573a5c43a9c64649786d287f8
Ben Rizk <coolguyben@hotmail.com>
2018-10-23 22:31:58 -0400

    started on welcome page

--- app/src/main/java/com/example/group/ondemandhomerepair/WelcomePage.java
+++ app/src/main/java/com/example/group/ondemandhomerepair/WelcomePage.java
@@ -2,6 +2,9 @@ package com.example.group.ondemandhomerepair;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class WelcomePage extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class WelcomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+        Spinner accountsList = (Spinner)findViewById(R.id.accountList); // find drop down and set as invisible
+        accountsList.setVisibility(View.GONE);
+        TextView userName = (TextView)findViewById(R.id.userField);
+        TextView userType = (TextView)findViewById(R.id.typeField);
+        userName.setText("username");           // username will go here gotten from log-in
+        userType.setText("usertype");           // type will go here gotten from log-in
+        if(userName.getText().equals("admin")){
+            accountsList.setVisibility(View.VISIBLE);
+        }
+
     }
 }
