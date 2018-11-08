1bd18172f95885e7d27a2b3cd96e131b7d168d5f
Ben Rizk <coolguyben@hotmail.com>
2018-11-08 16:36:22 -0500

    began working on deletion of services, work still needs to be done

--- app/src/main/java/com/example/group/ondemandhomerepair/DeletingServicePage.java
+++ app/src/main/java/com/example/group/ondemandhomerepair/DeletingServicePage.java
@@ -1,8 +1,25 @@
 package com.example.group.ondemandhomerepair;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+import java.util.*;
+
 
 public class DeletingServicePage extends AppCompatActivity {
 
@@ -11,6 +28,35 @@ public class DeletingServicePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deleting_service_page);
 
+        Spinner serviceList = (Spinner)findViewById(R.id.servicesSpinner);
+        final List<String> services = new ArrayList<String>();                    // create a list and fill the spinner with list contents
+
+       FirebaseDatabase.getInstance().getReference().child("services").addListenerForSingleValueEvent( // fill the list with services
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //Get map of users in datasnapshot
+                        for (DataSnapshot dsp : dataSnapshot.getChildren()) {
+                            services.add(String.valueOf(dsp.getValue()));
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //handle databaseError
+                    }
+                });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(                // fill the spinner in the page with contents
+                this, android.R.layout.simple_spinner_item, services);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        serviceList.setAdapter(adapter);
+
         Button deletingButton = (Button)findViewById(R.id.deletingServiceButt);
+        deletingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                // method for deleting from firbase here
+            }
+        });
     }
 }
