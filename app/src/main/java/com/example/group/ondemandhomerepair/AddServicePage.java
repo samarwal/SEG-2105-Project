b9652968dea4604acbc09ea4f132fd9a1fb92b69
Theo VdB <theo.a.vdb@gmail.com>
2018-11-08 15:23:34 -0500

    adding services now works

--- app/src/main/java/com/example/group/ondemandhomerepair/AddServicePage.java
+++ app/src/main/java/com/example/group/ondemandhomerepair/AddServicePage.java
@@ -1,15 +1,23 @@
 package com.example.group.ondemandhomerepair;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddServicePage extends AppCompatActivity {
 
@@ -33,9 +41,36 @@ public class AddServicePage extends AppCompatActivity {
         rate = findViewById(R.id.serviceWageText);
         addingServiceButt = findViewById(R.id.addingServiceButt);
 
-        if (statusValidate()){
+        addingServiceButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if (statusValidate()){
+                    final String Name = name.getText().toString().trim();
+                    final String Rate = rate.getText().toString().trim();
 
-        }
+                    Service user = new Service(
+                            Name,
+                            Integer.parseInt(Rate)
+                    );
+
+                    FirebaseDatabase.getInstance().getReference("Services")
+                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(AddServicePage.this, "Service addition successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(AddServicePage.this, MainActivity.class));
+                            } else {
+                                Toast.makeText(AddServicePage.this, "Registration f", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+
     }
 
     public boolean statusValidate() {
