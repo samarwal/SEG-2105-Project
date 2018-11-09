aa60ff3f79be7b80b6ab3ed593443416ebf72deb
Miguel Garzón <john.bousfield@hotmail.ca>
2018-11-09 08:00:30 -0500

    Most of EditService besides database reading and spinner reset

--- app/src/main/java/com/example/group/ondemandhomerepair/EditServicePage.java
+++ app/src/main/java/com/example/group/ondemandhomerepair/EditServicePage.java
@@ -3,10 +3,20 @@ package com.example.group.ondemandhomerepair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class EditServicePage extends AppCompatActivity {
     private Button editServiceButt;
@@ -14,7 +24,9 @@ public class EditServicePage extends AppCompatActivity {
     private EditText name;
     private EditText rate;
     private TextView errorMessage;
-    Spinner serviceList;
+    private Spinner serviceList;
+    List<String> services;
+    String id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +37,62 @@ public class EditServicePage extends AppCompatActivity {
         errorMessage = findViewById(R.id.errorMessageEdit);
         editServiceButt = findViewById(R.id.editServiceButt);
         serviceList = (Spinner)findViewById(R.id.spinner2);
+        services = new ArrayList<String>();
+        id = "";
+        final DatabaseReference serviceRef;
+
+        //Note This is the Spinner Fill From Bens Code
+        FirebaseDatabase.getInstance().getReference().child("services").addListenerForSingleValueEvent( // fill the list with services
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //Get map of users in datasnapshot
+                        for (DataSnapshot dsp : dataSnapshot.getChildren()) {
+                            services.add(String.valueOf(dsp.getValue()));
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //handle databaseError
+                    }
+                });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(                // fill the spinner in the page with contents
+                this, android.R.layout.simple_spinner_item, services);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        serviceList.setAdapter(adapter);
+        //End of Spinner fill
+
+
+        //THIS IS THE MAIN SECTION THAT NEEDS WORK. A BUTTON OR EVENT TO RELOAD REDO FOLLOWING EVERY TIME SPINNER IS CHANGED.
+        //RETREIVAL OF DATA FROM FIREBASE REFERENCES NEEDS TO BE IMPLEMENTED. DIRECT getValue DOES NOT EXIST.
+        //EVENTLISTENERS ARE CONFUSING
+        String id = String.valueOf(serviceList.getSelectedItem());
+        serviceRef = FirebaseDatabase.getInstance().getReference("service").child(id);
+        //DatabaseReference nameRef = serviceRef.child("serviceName");
+        //DatabaseReference wageRef = serviceRef.child("hourlyRate");
+        //service = new Service(nameRef,wageRef);
+        //name.setText(service.getServiceName());
+        //rate.setText(service.getHourlyRate());
+
+        //
+
 
         editServiceButt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 if (statusValidate()){
-                //push servuce to firebase
+                   service.setServiceName(name.getText().toString().trim());
+                   service.setHourlyRate(Integer.valueOf(rate.getText().toString().trim()));
+                   serviceRef.child("serviceName").setValue(service.getServiceName());
+                   serviceRef.child("hourlyRate").setValue(service.getHourlyRate());
                 }
             }
         });
     }
 
     public boolean statusValidate(){
-            if (service == null){
+            if (id == ""){
                 errorMessage.setText("! service was not chosen");
                 return false;
             }
