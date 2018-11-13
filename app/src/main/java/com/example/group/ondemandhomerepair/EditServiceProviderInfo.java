a57acad43fe4afd80b28b932a567bbd785e10564
Theo VdB <theo.a.vdb@gmail.com>
2018-11-13 17:38:35 -0500

    making activities for deliverable 3

---
+++ app/src/main/java/com/example/group/ondemandhomerepair/EditServiceProviderInfo.java
@@ -0,0 +1,13 @@
+package com.example.group.ondemandhomerepair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditServiceProviderInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_service_provider_info);
+    }
+}
---
+++ app/src/main/java/com/example/group/ondemandhomerepair/editServiceProviderServices.java
@@ -0,0 +1,14 @@
+package com.example.group.ondemandhomerepair;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class editServiceProviderServices extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_service_provider_services);
+    }
+
+}
