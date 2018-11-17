4ed5afc94018b38a6abd5ff61c4a5b6a0d64c137
Ben Rizk <coolguyben@hotmail.com>
2018-11-17 14:53:29 -0500

    added input components to editProviderInfo page

--- app/src/main/java/com/example/group/ondemandhomerepair/EditServiceProviderInfo.java
+++ app/src/main/java/com/example/group/ondemandhomerepair/EditServiceProviderInfo.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import static com.example.group.ondemandhomerepair.LogIn.EXTRA_TEXT1;
 
@@ -16,7 +18,14 @@ public class EditServiceProviderInfo extends AppCompatActivity {
         setContentView(R.layout.activity_edit_service_provider_info);
 
         final Intent intent = getIntent();
-        final String providerName = intent.getStringExtra(EXTRA_TEXT1);  // retrieve provider name from welcome page intent
+        final String providerUser = intent.getStringExtra(EXTRA_TEXT1);  // retrieve provider name from welcome page intent
+
+        EditText providerCompany = (EditText)findViewById(R.id.providerNameText);   // all input components from page
+        EditText providerPhone = (EditText)findViewById(R.id.providerPhoneText);
+        EditText providerAddress = (EditText)findViewById(R.id.providerAddressText);
+        EditText providerDescription = (EditText)findViewById(R.id.providerDescriptText);
+        RadioGroup isLicensed = (RadioGroup)findViewById(R.id.checkLicenserb);
+
         Button addService = (Button)findViewById(R.id.providerAddServiceButt);
         Button changeTimes = (Button)findViewById(R.id.changeTimesButt);
 
@@ -24,7 +33,7 @@ public class EditServiceProviderInfo extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(EditServiceProviderInfo.this, editServiceProviderServices.class);
-                intent.putExtra(EXTRA_TEXT1, providerName);
+                intent.putExtra(EXTRA_TEXT1, providerUser);
                 startActivity(intent);
             }
         });
