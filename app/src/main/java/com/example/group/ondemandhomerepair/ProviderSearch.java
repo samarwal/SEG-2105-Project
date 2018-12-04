bf683ff6ff3d8b7fdfcd5912683852713f5448e3
Ben Rizk <coolguyben@hotmail.com>
2018-12-04 01:08:37 -0500

    fixed weird things added by git with last commit

--- app/src/main/java/com/example/group/ondemandhomerepair/ProviderSearch.java
+++ app/src/main/java/com/example/group/ondemandhomerepair/ProviderSearch.java
@@ -7,12 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-<<<<<<< HEAD
+
 import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
-=======
->>>>>>> 3229af33631a0d0ae840f4fb6e878abd3e9ff97f
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -20,24 +19,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
-<<<<<<< HEAD
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-=======
->>>>>>> 3229af33631a0d0ae840f4fb6e878abd3e9ff97f
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-<<<<<<< HEAD
+
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-=======
->>>>>>> 3229af33631a0d0ae840f4fb6e878abd3e9ff97f
+
 
 public class ProviderSearch extends AppCompatActivity {
 
@@ -56,19 +51,16 @@ public class ProviderSearch extends AppCompatActivity {
         mUserDatabase = FirebaseDatabase.getInstance().getReference("Services");
 
         mSearchField = (EditText) findViewById(R.id.search_field);
-<<<<<<< HEAD
-        mSearchBtn = (Button) findViewById(R.id.button2);
+
+        mSearchBtn = (Button) findViewById(R.id.addTime);
         mSearchByRate = (Button)findViewById(R.id.rateSearchButt);
         mSearchForProvider = (Button)findViewById((R.id.button5));
         mResultList = (ListView) findViewById(R.id.listings);
 //        mResultList.setHasFixedSize(true);
 //        mResultList.setLayoutManager(new LinearLayoutManager(this));
-=======
-        mSearchBtn = (Button) findViewById(R.id.addTime);
-        mResultList = (RecyclerView) findViewById(R.id.result_list);
-        mResultList.setHasFixedSize(true);
-        mResultList.setLayoutManager(new LinearLayoutManager(this));
->>>>>>> 3229af33631a0d0ae840f4fb6e878abd3e9ff97f
+//        mSearchBtn = (Button) findViewById(R.id.addTime);
+//        mResultList = (RecyclerView) findViewById(R.id.result_list);
+
 
 
         mSearchBtn.setOnClickListener(new View.OnClickListener() {
@@ -193,6 +185,7 @@ public class ProviderSearch extends AppCompatActivity {
                                for (DataSnapshot dsp2 : dataSnapshot.child(dsp.getKey()).child("myServices").getChildren()) {   // search through found providers services
                                    if(String.valueOf(dsp2.child("serviceName").getValue()).equals(searchedServiceName)){   // does provider provide service searched for?
                                        getRatingsList.add(String.valueOf(dsp.child("username").getValue()) + " " +String.valueOf(dsp2.child("rating").getValue()));    // add provider name and rating to list
+                                        //TODO ratings need to be added to database to be searched
                                    }
                                }
                            }
